32163326cce38a3c2b1e7bc6bc4d7fa0fca7d8c4
Daviti Oghiashvili <daviti.oghiashvili@leverx.com>
2025-07-21 15:19:23 +0400

    added reuse library

--- app/free-style-sapui5-app/webapp/controller/ListReport.controller.js
+++ app/free-style-sapui5-app/webapp/controller/ListReport.controller.js
@@ -9,8 +9,7 @@ sap.ui.define(
     '../utils/Constants',
     'sap/ui/core/BusyIndicator',
     'sap/m/MessageToast',
-    'sap/ui/core/Messaging',
-    "sap/fe/navigation/library"
+    'sap/ui/core/Messaging'
   ],
   (
     BaseController,
@@ -22,8 +21,7 @@ sap.ui.define(
     Constants,
     BusyIndicator,
     MessageToast,
-    Messaging,
-    feNavigation
+    Messaging
   ) => {
     'use strict';
     return BaseController.extend('freestylesapui5app.controller.ListReport', {
